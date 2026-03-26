477997a855f6545a6540b05a74adc0f9f529779b
jmariodev <jm.gonzalezq02@gmail.com>
2026-03-26 07:59:39 -0500

    Initialize Spring Boot project "platos-tipicos-sb-api"

---
+++ src/main/java/com/ucompensar/platostipicossbapi/PlatosTipicosSbApiApplication.java
@@ -0,0 +1,13 @@
+package com.ucompensar.platostipicossbapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatosTipicosSbApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatosTipicosSbApiApplication.class, args);
+    }
+
+}
